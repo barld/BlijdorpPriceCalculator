dfeec6cce9f0eeb1bd3c400836d51449b2e295c9
barld <bakeds@gmail.com>
2016-05-18 12:31:39 +0200

    create frame for online calculator

---
+++ BlijdorpPriceCalculator/ConsoleInput.fs
@@ -0,0 +1,9 @@
+﻿module ConsoleInput
+    
+let GetMinInt message (onInvalid:Unit->'a) (onValid:int->'a) (minValue:int) : 'a =
+    printfn message
+    match System.Int32.TryParse(System.Console.ReadLine()) with
+    | false,_ -> onInvalid ()
+    | true, n when n<minValue -> onInvalid ()
+    | true, n -> onValid n
+
---
+++ BlijdorpPriceCalculator/Online.fs
@@ -0,0 +1,19 @@
+﻿module Online
+
+open ConsoleInput
+
+type NoteInput =
+    {
+        Addults:int
+        Childs:int
+    }
+
+let collectInput () =
+    let rec aInput () = GetMinInt "how much addults? (from age of 13)" aInput (fun n -> n) 1
+    let rec cInput () = GetMinInt "how much children?" cInput (fun n -> n) 0
+    {
+        Addults = aInput ()
+        Childs = cInput ()
+    }
+
+let calculateNote (ni:NoteInput) = float32 ni.Addults * 20.f + float32 ni.Childs * 15.5f
